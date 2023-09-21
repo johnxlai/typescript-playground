a11db2c71a0269c52ab62d87c89f4ee7d2dfef56
John Lai <lai.john.py@gmail.com>
2023-09-21 10:15:00 -0400

    Typles

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,10 @@
-let age: number = 20;
-if (age < 50)
-  age += 10;
-console.log(age);
+let sales = 123_456_5789;
+let course = 'TypeScript';
+let is_published: true;
+let level;
+let numbers: number[] = [];
+
+
+// tuples
+//1, 'Mosh'
+let user:[number, string] = [1, 'mosh'];
