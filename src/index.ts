cda677ed801283931e7db593d65befe4b3e844ac
John Lai <lai.john.py@gmail.com>
2023-09-21 10:48:31 -0400

    Add objectgs

--- src/index.ts
+++ src/index.ts
@@ -14,3 +14,19 @@ let user:[number, string] = [1, 'mosh'];
 const enum Size { Small = 1, Medium, Large}
 let mySize: Size = Size.Medium;
 console.log(mySize);
+
+//functions
+function calculateTax(income:number):number {
+  if (income < 50_000)
+    return income * 1.2;
+
+  return income * 1.3;
+}
+
+//Objects
+let employee: {
+  readonly id:number,
+  name:string,
+  retire:(date: Date) => void;
+} = { id: 1, name: 'Mosh'};
+
