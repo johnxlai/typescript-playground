14de7fd489017bf8723094e4b25d53e817b69cb0
John Lai <lai.john.py@gmail.com>
2023-10-13 16:27:35 -0400

    add inheritance

--- src/index.ts
+++ src/index.ts
@@ -188,3 +188,32 @@ class Ride {
 let ride1 = new Ride();
 ride1.start()
 console.log(Ride.activeRides);
+
+
+//Inheritance
+class Person {
+  constructor(public firstName: string, public lastName: string) {
+  }
+  get fullName() {
+    return this.firstName + ' ' + this.lastName;
+  }
+
+  walk() {
+    console.log('walking');
+  }
+}
+
+class Student extends Person {
+  constructor( public studentId: number, firstName: string, lastName: string) {
+    super(firstName, lastName);
+  }
+  takeTest() {
+    console.log('taking test');
+  }
+}
+
+
+let student = new Student(1, 'John', 'john@gmail.com');
+student.takeTest();
+
+
